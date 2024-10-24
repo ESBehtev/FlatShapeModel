d603dc0418a66accdd64bc0b98dd32ab5b7dc447
ES <55274511+ESBehtev@users.noreply.github.com>
2024-10-24 22:17:35 +0300

    first step

---
+++ FlatShapeModel/FlatShapeModel/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  FlatShapeModel
+//
+//  Created by Эдвард on 24.10.2024.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
